a4e35305490c01113c7091fab58ad9bb1062a868
Peyton Kellogg <pekellogg@hotmail.com>
2022-09-29 17:04:50 -0700

    commit tree d0276156f35ab6621483e5f428df597414619788

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,24 @@
-// your Bomb code here!
+import React from 'react';
+
+class Bomb extends React.Component {
+
+  constructor(props) {
+    super();
+    this.state = { secondsLeft: props.initialCount };
+  };
+
+  // decrement = () => {
+  //   const newSecondsLeft = this.state.secondsLeft - 1;
+  //   this.setState({ secondsLeft: newSecondsLeft });
+  // };
+
+  render() {
+    const msg = this.state.secondsLeft === 0 ? `Boom!` : `${this.state.secondsLeft} seconds left before I go boom!`;
+    return (
+      <div>{msg}</div>
+    )
+  }
+
+}
+
+export default Bomb;
